b6f9581a125250cabe7b1e9076b9dcc43374e102
Jenin-Joy <jeninjoy21@gmail.com>
2024-10-14 16:14:42 +0530

    list view

--- grid_builder/lib/product_grid.dart
+++ grid_builder/lib/product_grid.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:grid_builder/list_view.dart';
 import 'package:grid_builder/mock_data.dart';
 
 class ProductGrid extends StatefulWidget {
@@ -13,53 +14,56 @@ class _ProductGridState extends State<ProductGrid> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Product Grid'),
+        title: const Text('Product Grid'),
       ),
-      body: GridView.builder(
-        padding: EdgeInsets.all(8.0),
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2, // Number of columns
-          childAspectRatio: 0.7, // Adjusts the aspect ratio of the items
-          crossAxisSpacing: 8.0,
-          mainAxisSpacing: 8.0,
-        ),
+      body: GridView.builder(gridDelegate:
+       const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2), 
         itemCount: mockProducts.length,
         itemBuilder: (context, index) {
           final product = mockProducts[index];
-          return Card(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Image.network(
-                  product['imageUrl'],
-                  fit: BoxFit.cover,
-                  height: 100,
-                  width: double.infinity,
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Text(
-                    product['name'],
-                    style: TextStyle(fontWeight: FontWeight.bold),
+          return
+          GestureDetector(
+            onTap: (){
+              Navigator.push(context, MaterialPageRoute(builder: (context) => ProductList(),));
+            },
+            child: (
+              
+              Card(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Image.asset(
+                    product['imageUrl'],
+                    fit: BoxFit.cover,
+                    height: 100,
+                    width: double.infinity,
                   ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                  child: Text(
-                    product['description'],
-                    maxLines: 2,
-                    overflow: TextOverflow.ellipsis,
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      product['name'],
+                      style: const TextStyle(fontWeight: FontWeight.bold),
+                    ),
                   ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Text('\$${product['price'].toStringAsFixed(2)}'),
-                ),
-              ],
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                    child: Text(
+                      product['description'],
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text('\$${product['price'].toStringAsFixed(2)}'),
+                  ),
+                ],
+              ),
+            )
             ),
           );
-        },
-      ),
+        },)
     );
   }
 }
