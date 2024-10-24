2e02c07c643e26a7fb018407708d3e0cefce7815
navendunandu <navendunandu@gmail.com>
2024-10-24 14:29:37 +0530

    list view

---
+++ list_view/lib/list_view.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/material.dart';
+
+class ListViewScreen extends StatelessWidget {
+  const ListViewScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView(
+        children: const [
+          ListTile(
+            title: Text('Item 1'),
+            subtitle: Text("Item 1's subtitle"),
+            leading: Icon(Icons.account_circle),
+            trailing: Icon(Icons.delete_forever_outlined),
+          ),
+          ListTile(
+            title: Text('Item 2'),
+            subtitle: Text("Item 2's subtitle"),
+            leading: Icon(Icons.account_circle),
+            trailing: Icon(Icons.delete_forever_outlined),
+          ),
+          ListTile(
+            title: Text('Item 3'),
+            subtitle: Text("Item 3's subtitle"),
+            leading: Icon(Icons.account_circle),
+            trailing: Icon(Icons.delete_forever_outlined),
+          ),
+          ListTile(
+            title: Text('Item 4'),
+            subtitle: Text("Item 4's subtitle"),
+            leading: Icon(Icons.account_circle),
+            trailing: Icon(Icons.delete_forever_outlined),
+          ),
+          ListTile(
+            title: Text('Item 5'),
+            subtitle: Text("Item 5's subtitle"),
+            leading: Icon(Icons.account_circle),
+            trailing: Icon(Icons.delete_forever_outlined),
+          ),
+          ListTile(
+            title: Text('Item 1'),
+            subtitle: Text("Item 1's subtitle"),
+            leading: Icon(Icons.account_circle),
+            trailing: Icon(Icons.delete_forever_outlined),
+          ),
+          ListTile(
+            title: Text('Item 2'),
+            subtitle: Text("Item 2's subtitle"),
+            leading: Icon(Icons.account_circle),
+            trailing: Icon(Icons.delete_forever_outlined),
+          ),
+          ListTile(
+            title: Text('Item 3'),
+            subtitle: Text("Item 3's subtitle"),
+            leading: Icon(Icons.account_circle),
+            trailing: Icon(Icons.delete_forever_outlined),
+          ),
+          ListTile(
+            title: Text('Item 4'),
+            subtitle: Text("Item 4's subtitle"),
+            leading: Icon(Icons.account_circle),
+            trailing: Icon(Icons.delete_forever_outlined),
+          ),
+          ListTile(
+            title: Text('Item 5'),
+            subtitle: Text("Item 5's subtitle"),
+            leading: Icon(Icons.account_circle),
+            trailing: Icon(Icons.delete_forever_outlined),
+          ),
+          ListTile(
+            title: Text('Item 1'),
+            subtitle: Text("Item 1's subtitle"),
+            leading: Icon(Icons.account_circle),
+            trailing: Icon(Icons.delete_forever_outlined),
+          ),
+          ListTile(
+            title: Text('Item 2'),
+            subtitle: Text("Item 2's subtitle"),
+            leading: Icon(Icons.account_circle),
+            trailing: Icon(Icons.delete_forever_outlined),
+          ),
+          ListTile(
+            title: Text('Item 3'),
+            subtitle: Text("Item 3's subtitle"),
+            leading: Icon(Icons.account_circle),
+            trailing: Icon(Icons.delete_forever_outlined),
+          ),
+          ListTile(
+            title: Text('Item 4'),
+            subtitle: Text("Item 4's subtitle"),
+            leading: Icon(Icons.account_circle),
+            trailing: Icon(Icons.delete_forever_outlined),
+          ),
+          ListTile(
+            title: Text('Item 5'),
+            subtitle: Text("Item 5's subtitle"),
+            leading: Icon(Icons.account_circle),
+            trailing: Icon(Icons.delete_forever_outlined),
+          ),
+        ],
+      ),
+    );
+  }
+}
