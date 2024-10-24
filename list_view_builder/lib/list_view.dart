d7557cb204539907a61a66499252f3b2591375a6
navendunandu <navendunandu@gmail.com>
2024-10-24 14:58:13 +0530

    list view builder

---
+++ list_view_builder/lib/list_view.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:list_view_builder/mock_data.dart';
+
+class ProductList extends StatefulWidget {
+  const ProductList({super.key});
+
+  @override
+  State<ProductList> createState() => _ProductListState();
+}
+
+class _ProductListState extends State<ProductList> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Product Grid'),
+      ),
+      body: ListView.builder(
+        itemCount: mockProducts.length,
+        itemBuilder: (context, index) {
+          final product = mockProducts[index];
+          return(
+            Container(
+              padding: EdgeInsets.all(8),
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.grey),
+              ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  children: [
+                    Image.asset(
+                      product['imageUrl'],
+                      fit: BoxFit.cover,
+                      width: 400,
+                    ),
+                    Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(
+                            product['name'],
+                            style: const TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                          child: Text(
+                            product['description'],
+                            maxLines: 2,
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text('\$${product['price'].toStringAsFixed(2)}'),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          )
+          );
+        },)
+    );
+  }
+}
